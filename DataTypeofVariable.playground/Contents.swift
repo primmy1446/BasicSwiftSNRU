44986b81a2c16e6c1a22f85f011283af48528981
Primmy1446 <48310@sakolraj.ac.th>
2019-03-18 14:28:14 +0700

    Start DataType

---
+++ DataTypeofVariable.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+//String Type คือตัวแปรที่เป็นอักษร
+
+var myName = "Doramon" // นี่คือการาประกาศ ที่อาศัยชนิดของ Value  บอก Datatype
+var mySurname: String = "AAAAAA"  //นี่คือการประกาศแบบ กำหนด Datatype ตั้งแต่เกิด
+
+
+
+
---
+++ VarialAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variable
+//การใส่คอมเม้นเมื่อตั้งชื่อตัวแปรเสร็จให้กดcommand/
+
+var  myName = "Prim"
+myName = "Conan"
+
+//Explicit Constant
+let myFriend = "Kid"
+myFriend = "Hibara"
+//myfriend = "Hibara" Error Because myfriend is let
